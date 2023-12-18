9315e74e57be21434c62685aa3c78bce9ea56a48
alekskazantsev <alekskazantsev@ozon.ru>
2023-12-18 23:44:46 +0300

    added startup

--- src/web/main.py
+++ src/web/main.py
@@ -6,21 +6,22 @@ def get_handler():
     handler = TextSummaryService()
     return handler
 
-st.title('Выделение основных тезисов из вашего текста')
-text = st.text_area(
-    label='',
-    placeholder='Введите текст',
-    key='target_text')
+def show():
+    st.title('Выделение основных тезисов из вашего текста')
+    text = st.text_area(
+        label='',
+        placeholder='Введите текст',
+        key='target_text')
 
-btn = st.button('Сформировать конспект', type='primary')
+    btn = st.button('Сформировать конспект', type='primary')
 
-if btn and text:
-    originalSummary, summary = get_handler().handle_ru(text)
-    #TODO сделать нормальный вывод
-    st.write('## Оригинальное саммари:', )
-    st.write(originalSummary)
-    st.write('## Итоговый конспект:')
-    st.write(summary)
+    if btn and text:
+        originalSummary, summary = get_handler().handle_ru(text)
+        #TODO сделать нормальный вывод
+        st.write('## Оригинальное саммари:', )
+        st.write(originalSummary)
+        st.write('## Итоговый конспект:')
+        st.write(summary)
     
     
 
